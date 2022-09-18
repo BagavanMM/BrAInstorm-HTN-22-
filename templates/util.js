3cd1d2718a47ce9dbec17efd90b32a03fb03f045
alexyev <85699087+alexyev@users.noreply.github.com>
2022-09-17 22:12:51 -0400

    cmt

--- templates/util.js
+++ templates/util.js
@@ -4,9 +4,9 @@ const brainwave = user.message;
 const alpha1 = ['../images/alpha1/alpha1.png', '../images/alpha1/alpha2.png', '../images/alpha1/alpha3.png']
 const beta = ['../images/beta/beta.png', '../images/beta/beta1.png']
 const delta = ['../images/delta/delta.png', '../images/delta/delta2.png']
-const hi_beta = ['../images/high-beta/high-beta.png', '../images/high-beta/high-beta1.png', '../images/high-beta/high-beta2.png', '../images/high-beta/high-beta3.png']
+const high_beta = ['../images/high-beta/high-beta.png', '../images/high-beta/high-beta1.png', '../images/high-beta/high-beta2.png', '../images/high-beta/high-beta3.png']
 const theta = ['../images/theta/theta.png', '../images/theta/theta1.png', '../images/theta/theta2.png']
-const lo_beta = ['../images/low-beta/low-beta.png','../images/low-beta/low-beta1.png']
+const low_beta = ['../images/low-beta/low-beta.png','../images/low-beta/low-beta1.png']
 
 const central = [['../images/high-beta/high-beta.png', '../images/high-beta/high-beta1.png', '../images/high-beta/high-beta2.png', '../images/high-beta/high-beta3.png'], ['../images/beta/beta.png', '../images/beta/beta1.png'],
 ['../images/low-beta/low-beta.png','../images/low-beta/low-beta1.png'], ['../images/alpha1/alpha1.png', '../images/alpha1/alpha2.png', '../images/alpha1/alpha3.png'], ['../images/theta/theta.png', '../images/theta/theta1.png', '../images/theta/theta2.png'],
@@ -15,8 +15,12 @@ const central = [['../images/high-beta/high-beta.png', '../images/high-beta/high
 //  go into central[brainwave], return a random image, set that image to getElbyId in result
 
 function rand (array) {
-    return Math.floor(Math.random() * array.length)
+return Math.floor(Math.random() * array.length)
 }
 
+let img = document.createElement("img");
+img.src = central[brainwave][rand(central[brainwave])];
+let src = document.getElementById("photos");
+src.appendChild(img);
 
-document.getElementById('results-picture') = central[brainwave][rand([central[brainwave]])]
+console.log(brainwave)
